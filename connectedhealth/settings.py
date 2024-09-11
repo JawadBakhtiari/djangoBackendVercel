12855dde7d6e800379b61675ffc17c6611f7fca1
Ahmad Jawad Bakhtiari <aj.bakhti@gmail.com>
2024-09-11 23:21:51 +1000

    Added elastic IP address to host

--- connectedhealth/settings.py
+++ connectedhealth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g_)1f8m@9&ad07ailm@r*g2woiza7)(ev&0jtg0l4au72!p(ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.150', '192.168.0.203', '13.236.186.232']
+ALLOWED_HOSTS = ['192.168.0.150', '192.168.0.203', 'ec2-3-104-215-22.ap-southeast-2.compute.amazonaws.com', '3.104.215.22']
 
 # Application definition
 
