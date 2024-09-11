59f7a44bbdf4d2c375816a839d8621c4bab5497f
Ahmad Jawad Bakhtiari <aj.bakhti@gmail.com>
2024-09-11 12:20:08 +1000

    Removed vercel changes

--- connectedhealth/settings.py
+++ connectedhealth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g_)1f8m@9&ad07ailm@r*g2woiza7)(ev&0jtg0l4au72!p(ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['192.168.0.150']
 
 # Application definition
 
